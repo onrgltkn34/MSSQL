32035f121d3ae5fa2b77d873b5912ca66ec6bf0f
onrgltkn34 <156789603+onrgltkn34@users.noreply.github.com>
2024-01-16 14:17:51 +0300

    Create Bekleyen_Kuponlar.sql

---
+++ MisliCalismalar/Bekleyen_Kuponlar.sql
@@ -0,0 +1,8 @@
+select * from EventCouponMasters with(nolock) where MemberId='32619253' and Status=0
+select * from HorseRaceCouponMasters with(nolock) where MemberId='32619253' and Status=0
+select * from SoccerPoolCouponMasters with(nolock) where MemberId='32619253' and Status=0
+
+-- 0 Bekleyen
+-- 1 Kazanan
+-- 2 Kaybeden
+-- 3 Ýptal
